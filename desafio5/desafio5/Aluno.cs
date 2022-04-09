0136865446610cb809903153daa7dd76ec08f90b
GutoSwan <gubuenosa@gmail.com>
2022-04-09 14:11:09 -0300

    Desafio 5 concluido

---
+++ desafio5/desafio5/Aluno.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace desafio5
+
+{
+    public class Aluno
+
+    {
+        public string nome;
+        public int idade;
+        public double nota;
+
+        public Aluno(string nome, int idade, double nota)
+        {
+
+            this.nome = nome;
+            this.idade = idade;
+            this.nota = nota;
+        }
+        public Aluno CriarAluno(Aluno aluno)
+        {
+            return new Aluno(aluno.nome, aluno.idade, aluno.nota);
+        }
+    }
+}
---
+++ desafio5/desafio5/Class1.cs
@@ -0,0 +1,24 @@
+﻿using desafio5;
+
+
+
+Aluno orlandoAluno = new Aluno("orlando", 12, 5);
+
+Aluno scobyAluno = new Aluno("scoby", 15, 4);
+
+Aluno chubsAluno = new Aluno("chubs", 23, 8);
+
+List<Aluno> alunos = new List<Aluno>();
+alunos.Add(orlandoAluno);
+alunos.Add(scobyAluno);
+alunos.Add(chubsAluno);
+
+
+StreamWriter sw = new StreamWriter("D:\\desafi C#\\desafio5\\alunosalunosNota.csv");
+sw.WriteLine("nome" + ";" + "idade" + ";" + "nota");
+foreach (var aluninho in alunos)
+{
+    sw.WriteLine(aluninho.nome + ";" + aluninho.idade + ";" + aluninho.nota);
+}
+sw.WriteLine("soma das notas: "+ ";" + alunos.Select(x => x.nota).Sum());
+sw.Close();
